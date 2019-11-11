0e7cec15916d645f773432a81ee65bc27d7c3a5d
Ryan Boggs <boggsrm@gmail.com>
2019-11-11 13:12:47 -0800

    Set the admin site headers

--- appts/admin.py
+++ appts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Workday, Service, ServiceCategory, Client, Appointment
 
+admin.site.site_header = "Chic Appointments Admin"
+admin.site.site_title = "Chic Appointments Admin Area"
+admin.site.index_title = "Welcome to the appointment admin area"
+
 # Register your models here.
 admin.site.register(Workday)
 admin.site.register(ServiceCategory)
