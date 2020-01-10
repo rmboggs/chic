04eec105fc1a1a1e3e8d72b41e7b6af3e77e167a
Ryan Boggs <boggsrm@gmail.com>
2020-01-09 16:50:53 -0800

    Finish initial reports

--- appts/views.py
+++ appts/views.py
@@ -3,12 +3,13 @@ from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
 from django.db.models import Q, Sum, Count
-from django.db.models.functions import TruncYear, TruncMonth, TruncQuarter, ExtractYear, ExtractQuarter
+from django.db.models.functions import TruncYear, TruncMonth, TruncQuarter, TruncDate, ExtractYear, ExtractQuarter
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Appointment, AppointmentForm
 import chic.settings
 from datetime import datetime, time, date
+import csv
 
 # Create your views here.
 def index(request):
@@ -121,68 +122,100 @@ def schedule(request):
     return render(request, 'appts/schedule.html', context)
 
 @login_required(login_url='/login/')
-def reports(request, report_type=None, criteria=None):
+def reports(request, report_type=None):
     report_template = 'appts/reports.html'
+
     if report_type is None:
         return render(request, report_template)
 
-    default_date = datetime.now()
-
-    raise Http404("Reports are not ready yet")
+    if request.method == 'POST':
+        criteria = request.POST.get('criteria')
+    else:
+        criteria = None
 
     # get the initial list of Appointments
-    appt_list = Appointment.objects.filter(Q(cash_sales__gt=0) | Q(credit_sales__gt=0)).annotate(year=ExtractYear('service_date'), month=TruncMonth('service_date'), quarter=ExtractQuarter('service_date')).values('year', 'month', 'quarter', 'service_date' 'cash_sales', 'credit_sales')
+    appt_list = Appointment.objects.filter(Q(cash_sales__gt=0) | Q(credit_sales__gt=0)).annotate(year=ExtractYear('service_date'), month=TruncMonth('service_date'), quarter=ExtractQuarter('service_date'), service_day=TruncDate('service_date')).values('year', 'month', 'quarter', 'service_day', 'service_date', 'cash_sales', 'credit_sales')
     report_type_l = report_type.lower()
     context = {'report_type': report_type_l}
+    default_date = datetime.now()
 
     if report_type_l == "day":
-        if criteria is not None:
-            try:
-                filter_date = datetime.strptime(criteria, "%Y-%m-%d")
-            except:
-                raise Http404("Could not parse criteria for %s report", report_type, criteria)
+
+        if criteria is not None and len(criteria) > 0:
+            filter_date = datetime.strptime(criteria, "%Y-%m-%d")
         else:
             filter_date = default_date.date()
 
-        filter_month = date(filter_date.year, filter_date.month, 1)
-        filtered_items = appt_list.filter(month__date=filter_month)
-        group_items = filtered_items.values('service_date').order_by('service_date')
+        filtered_items = appt_list.filter(service_date__year=filter_date.year).filter(service_date__month=filter_date.month)
+        group_items = filtered_items.values('service_day').order_by('service_day')
 
+        context['search_value'] = filter_date.strftime("%Y-%m-%d")
         context['report_name'] = "Appointments by Date"
-
     elif report_type_l == "month":
-        filter_year = default_date.year
-        if criteria is not None:
+        if criteria is not None and len(criteria) > 0:
             try:
                 filter_year = int(criteria)
             except:
-                raise Http404("Could not parse year value from criteria: %s", criteria)
+                raise Http404("criteria is not numeric")
+        else:
+            filter_year = default_date.year
 
         filtered_items = appt_list.filter(year=filter_year)
         group_items = filtered_items.values('month').order_by('month')
 
+        context['search_value'] = str(filter_year)
         context['report_name'] = "Appointments by month"
     elif report_type_l == 'quarter':
-        filter_year = default_date.year
-        if criteria is not None:
+        if criteria is not None and len(criteria) > 0:
             try:
                 filter_year = int(criteria)
             except:
-                raise Http404("Could not parse year value from criteria: %s", criteria)
+                raise Http404("criteria is not numeric")
+        else:
+            filter_year = default_date.year
 
         filtered_items = appt_list.filter(year=filter_year)
-        group_items = filtered_items.values('quarter').order_by('quarter')
+        group_items = filtered_items.values('year', 'quarter').order_by('year', 'quarter')
 
+        context['search_value'] = str(filter_year)
         context['report_name'] = "Appointments by quarter"
     elif report_type_l == "year":
         filtered_items = appt_list
         group_items = filtered_items.values('year').order_by('year')
-        
+
         context['report_name'] = "Appointments by year"
     else:
         raise Http404("Unrecognized report type: %s" % (report_type,))
 
-    context['totals'] = filtered_items.aggregate(appt_count=Count('id'), cash=Sum('cash_sales'), credit=Sum('credit_sales'), total=Sum('cash_sales')+Sum('credit_sales'))
-    context['items'] = group_items.annotate(appt_count=Count('id'), cash=Sum('cash_sales'), credit=Sum('credit_sales'), total=Sum('cash_sales')+Sum('credit_sales'))
+    items = group_items.annotate(appt_count=Count('id'), cash=Sum('cash_sales'), credit=Sum('credit_sales'), total=Sum('cash_sales')+Sum('credit_sales'))
+    totals = filtered_items.aggregate(appt_count=Count('id'), cash=Sum('cash_sales'), credit=Sum('credit_sales'), total=Sum('cash_sales')+Sum('credit_sales'))
 
-    return render(response, report_template, context)
+    if request.method == 'POST':
+        action = request.POST.get('action')
+
+        if action == "Export":
+            # Create the HttpResponse object with the appropriate CSV header.
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename="%s_%s.csv"' % (report_type_l, context['search_value'])
+
+            writer = csv.writer(response)
+            header = [report_type_l, "Count", "Cash Sales", "Credit Sales", "Total Sales"]
+
+            writer.writerow(header)
+
+            for item in items:
+                writer.writerow(list(item.values()))
+
+            tmp_list = ["Grand total"]
+            tmp_list.append(totals["appt_count"])
+            tmp_list.append(totals["cash"])
+            tmp_list.append(totals["credit"])
+            tmp_list.append(totals["total"])
+            writer.writerow(tmp_list)
+
+            return response
+
+    context['totals'] = totals
+    context['items'] = items
+
+    return render(request, report_template, context)
